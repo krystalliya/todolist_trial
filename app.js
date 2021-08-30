f2360f8361c5053dd49b6664900568f3e1596e6a
zhou man <zhouman@zhous-MacBook.local>
2021-08-30 14:45:53 +0800

    added css first commit

--- app.js
+++ app.js
@@ -2,29 +2,30 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https");
 
 const app = express();
 
-app.set("view engine", "ejs");  //tell the app to use ejs as a view engine
+app.set("view engine", "ejs"); //tell the app to use ejs as a view engine
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+// to tell the server to use the static resources
 app.use(express.static("public"));
 
-var todos = [
-  {
+var todos = [{
     id: 1,
     event: "Buy cat food",
     dueDate: "End August",
     isCompleted: false
-  }, 
+  },
   {
     id: 2,
     event: "Feed Cats",
     dueDate: "mid August",
     isCompleted: false
-  }, 
+  },
   {
     id: 3,
     event: "Refill Water",
@@ -34,40 +35,30 @@ var todos = [
 ];
 
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
-  var url = "https://api.openweathermap.org/data/2.5/weather?q=singapore&appid=63d037e35559d27a0cb72e9538af1f24&units=metric";
-  https.get(url, function(response){
-    console.log(response.statusCode);
+  var today = new Date();
 
-    response.on("data", function(data){
-      const weatherData = JSON.parse(data);
-      var temp = weatherData.main.temp;
-      var weatherDes = weatherData.weather[0].main;
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "short",
+    // year: "numeric"
+  };
 
-////////。。。。分割线。。。。////////
-      var today = new Date();
+  var day = today.toLocaleDateString("en-us", options);
 
-      var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "short",
-        year: "numeric"
-      };
-
-      var day = today.toLocaleDateString("en-us", options);
-
-      //response by rendering a file called list which inside the views folder
-      //then has to have the extention .ejs
-      //into the list passing in a single variable: kindOfDay
-      //the value of the variable: day
-      res.render("list", {theDate: day, todoList: todos, temperature: temp, weather: weatherDes});
-    });
+  //response by rendering a file called list which inside the views folder
+  //then has to have the extention .ejs
+  //into the list passing in a single variable: kindOfDay
+  //the value of the variable: day
+  res.render("list", {
+    theDate: day,
+    todoList: todos
   });
-
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
 
   //to get the input data
   var event = req.body.newEvent;
@@ -75,7 +66,7 @@ app.post("/", function(req, res){
 
   // create an object to push to the array
   var eventObject = {
-    id: todos[todos.length - 1].id + 1,
+    id: todos[todos.length - 1].id + 1, //the id of the last item in the todoarray's + 1 is the new object's id 
     event: event,
     dueDate: dueDate,
     isCompleted: false
@@ -87,11 +78,13 @@ app.post("/", function(req, res){
 
 })
 
-app.post("/delete/:id", function(req, res){
+//the delete button function
+app.post("/delete/:id", function (req, res) {
   // console.log(req.params);
 
-  // to check the index of the delete item based on it's id in the array of the objects
-  var deleteIndex = todos.findIndex(function(todo){
+  //to check the index of the delete item based on 
+  //it's id in the array of the objects
+  var deleteIndex = todos.findIndex(function (todo) {
     return todo.id === parseInt(req.params.id) //change the string to number by parseInt
   })
 
@@ -101,6 +94,6 @@ app.post("/delete/:id", function(req, res){
   res.redirect("/");
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
   console.log(`The server is running on port ${process.env.PORT || 3000}.`);
 })
